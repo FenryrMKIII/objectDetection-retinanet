b9f0ec2f5165873e153d527814bfc7cb0adaa294
Bulle Jérémy <DAA426@engie.com>
2020-01-17 11:31:04 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='P&ID symbol detection using retinanet',
+    author='FenryrMKIII',
+    license='MIT',
+)
